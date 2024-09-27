d67b56043bb16425ad44e3cfdb2057fb0cf7a876
Voidztorm <rhawks@mail.pima.edu>
2024-09-26 17:38:26 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -3,4 +3,4 @@
 # Prints a simple short story
 print('Khrono is 21 years old and is an eminator of time, being the time emanator means he can control time, but only to an extent. The more he evolves as an emanator, the more powers he will possess.')
 print('The first power is a choice granted to the time emanators that they can choose for themselves and Khrono has chosen “omen” which allows him to see into the future possible outcomes by being isolated or sleeping.')
-print('He lives with his father, Aios, who over shelters Khrono and tells him to not use his powers for reasons he doesn't know. At the time being, Khrono has unlocked 5 of the 12 abilities that he may possess.')
+print('He lives with his father, Aios, who over shelters Khrono and tells him to not use his powers for reasons he doesn't know. At the time being, Khrono has unlocked five of the twelve abilities that he may possess.')
